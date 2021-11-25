294f26eac4753fdc973137488b8f401f9fbc53ad
OtavioLRS <otavioleitesantos@hotmail.com>
2021-11-25 00:46:53 -0300

    heroku verification

--- config.py
+++ config.py
@@ -11,7 +11,6 @@ class Config:
     SECRET_KEY = environ.get('SECRET_KEY')
     FLASK_ENV = environ.get('FLASK_ENV')
 
-    # SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI_LOCAL")
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
